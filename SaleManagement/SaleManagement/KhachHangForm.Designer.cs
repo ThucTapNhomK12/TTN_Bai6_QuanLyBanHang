1e5cb529569b9e29fe272a51357b496bb7210252
Phucnguyen021295 <phucnguyen021295@gmail.com>
2017-05-12 00:01:05 +0700

    Hoan thanh form khach hang

---
+++ SaleManagement/SaleManagement/KhachHangForm.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace SaleManagement
+{
+    partial class KhachHangForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSoDienThoai = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX5 = new DevComponents.DotNetBar.LabelX();
+            this.txtDiaChi = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX4 = new DevComponents.DotNetBar.LabelX();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.txtHoten = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.btnExit = new DevComponents.DotNetBar.ButtonX();
+            this.btnSubmit = new DevComponents.DotNetBar.ButtonX();
+            this.dtNgaySinh = new DevComponents.Editors.DateTimeAdv.DateTimeInput();
+            ((System.ComponentModel.ISupportInitialize)(this.dtNgaySinh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSoDienThoai
+            // 
+            // 
+            // 
+            // 
+            this.txtSoDienThoai.Border.Class = "TextBoxBorder";
+            this.txtSoDienThoai.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtSoDienThoai.Location = new System.Drawing.Point(137, 201);
+            this.txtSoDienThoai.Name = "txtSoDienThoai";
+            this.txtSoDienThoai.PreventEnterBeep = true;
+            this.txtSoDienThoai.Size = new System.Drawing.Size(203, 20);
+            this.txtSoDienThoai.TabIndex = 17;
+            // 
+            // labelX5
+            // 
+            // 
+            // 
+            // 
+            this.labelX5.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX5.Location = new System.Drawing.Point(35, 201);
+            this.labelX5.Name = "labelX5";
+            this.labelX5.Size = new System.Drawing.Size(87, 22);
+            this.labelX5.TabIndex = 16;
+            this.labelX5.Text = "Số điện thoại";
+            // 
+            // txtDiaChi
+            // 
+            // 
+            // 
+            // 
+            this.txtDiaChi.Border.Class = "TextBoxBorder";
+            this.txtDiaChi.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtDiaChi.Location = new System.Drawing.Point(137, 159);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.PreventEnterBeep = true;
+            this.txtDiaChi.Size = new System.Drawing.Size(203, 20);
+            this.txtDiaChi.TabIndex = 15;
+            // 
+            // labelX4
+            // 
+            // 
+            // 
+            // 
+            this.labelX4.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX4.Location = new System.Drawing.Point(35, 159);
+            this.labelX4.Name = "labelX4";
+            this.labelX4.Size = new System.Drawing.Size(87, 22);
+            this.labelX4.TabIndex = 14;
+            this.labelX4.Text = "Địa chỉ";
+            // 
+            // labelX3
+            // 
+            // 
+            // 
+            // 
+            this.labelX3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX3.Location = new System.Drawing.Point(35, 118);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(87, 20);
+            this.labelX3.TabIndex = 12;
+            this.labelX3.Text = "Ngày sinh";
+            // 
+            // txtHoten
+            // 
+            // 
+            // 
+            // 
+            this.txtHoten.Border.Class = "TextBoxBorder";
+            this.txtHoten.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtHoten.Location = new System.Drawing.Point(137, 75);
+            this.txtHoten.Name = "txtHoten";
+            this.txtHoten.PreventEnterBeep = true;
+            this.txtHoten.Size = new System.Drawing.Size(203, 20);
+            this.txtHoten.TabIndex = 11;
+            // 
+            // labelX2
+            // 
+            // 
+            // 
+            // 
+            this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX2.Location = new System.Drawing.Point(35, 75);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(87, 22);
+            this.labelX2.TabIndex = 10;
+            this.labelX2.Text = "Họ tên";
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX1.Location = new System.Drawing.Point(91, 25);
+            this.labelX1.Margin = new System.Windows.Forms.Padding(4);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(210, 28);
+            this.labelX1.TabIndex = 9;
+            this.labelX1.Text = "Thông tin khách hàng";
+            // 
+            // btnExit
+            // 
+            this.btnExit.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnExit.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnExit.Location = new System.Drawing.Point(252, 247);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(87, 31);
+            this.btnExit.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnExit.TabIndex = 20;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnSubmit.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnSubmit.Location = new System.Drawing.Point(137, 247);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(87, 31);
+            this.btnSubmit.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnSubmit.TabIndex = 19;
+            this.btnSubmit.Text = "Xác nhận";
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // dtNgaySinh
+            // 
+            // 
+            // 
+            // 
+            this.dtNgaySinh.BackgroundStyle.Class = "DateTimeInputBackground";
+            this.dtNgaySinh.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dtNgaySinh.ButtonDropDown.Shortcut = DevComponents.DotNetBar.eShortcut.AltDown;
+            this.dtNgaySinh.ButtonDropDown.Visible = true;
+            this.dtNgaySinh.IsPopupCalendarOpen = false;
+            this.dtNgaySinh.Location = new System.Drawing.Point(137, 118);
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            this.dtNgaySinh.MonthCalendar.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dtNgaySinh.MonthCalendar.CalendarDimensions = new System.Drawing.Size(1, 1);
+            this.dtNgaySinh.MonthCalendar.ClearButtonVisible = true;
+            // 
+            // 
+            // 
+            this.dtNgaySinh.MonthCalendar.CommandsBackgroundStyle.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground2;
+            this.dtNgaySinh.MonthCalendar.CommandsBackgroundStyle.BackColorGradientAngle = 90;
+            this.dtNgaySinh.MonthCalendar.CommandsBackgroundStyle.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground;
+            this.dtNgaySinh.MonthCalendar.CommandsBackgroundStyle.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.dtNgaySinh.MonthCalendar.CommandsBackgroundStyle.BorderTopColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarDockedBorder;
+            this.dtNgaySinh.MonthCalendar.CommandsBackgroundStyle.BorderTopWidth = 1;
+            this.dtNgaySinh.MonthCalendar.CommandsBackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dtNgaySinh.MonthCalendar.DisplayMonth = new System.DateTime(2017, 4, 1, 0, 0, 0, 0);
+            // 
+            // 
+            // 
+            this.dtNgaySinh.MonthCalendar.NavigationBackgroundStyle.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.dtNgaySinh.MonthCalendar.NavigationBackgroundStyle.BackColorGradientAngle = 90;
+            this.dtNgaySinh.MonthCalendar.NavigationBackgroundStyle.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.dtNgaySinh.MonthCalendar.NavigationBackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.dtNgaySinh.MonthCalendar.TodayButtonVisible = true;
+            this.dtNgaySinh.Name = "dtNgaySinh";
+            this.dtNgaySinh.Size = new System.Drawing.Size(203, 20);
+            this.dtNgaySinh.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.dtNgaySinh.TabIndex = 21;
+            // 
+            // KhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 314);
+            this.Controls.Add(this.dtNgaySinh);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtSoDienThoai);
+            this.Controls.Add(this.labelX5);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.labelX4);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.txtHoten);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.labelX1);
+            this.Name = "KhachHang";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Khách hàng";
+            ((System.ComponentModel.ISupportInitialize)(this.dtNgaySinh)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.Controls.TextBoxX txtSoDienThoai;
+        private DevComponents.DotNetBar.LabelX labelX5;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtDiaChi;
+        private DevComponents.DotNetBar.LabelX labelX4;
+        private DevComponents.DotNetBar.LabelX labelX3;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtHoten;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.ButtonX btnExit;
+        private DevComponents.DotNetBar.ButtonX btnSubmit;
+        private DevComponents.Editors.DateTimeAdv.DateTimeInput dtNgaySinh;
+    }
+}
---
+++ SaleManagement/SaleManagement/KhachHangForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaleManagement
+{
+    public partial class KhachHangForm : Form
+    {
+        db_sale_managementEntities db = new db_sale_managementEntities();
+
+        public khach_hang selectedCustomer = QuanLy.selectedCustomer;
+        public KhachHangForm()
+        {
+            InitializeComponent();
+
+            if (selectedCustomer != null)
+            {
+                txtHoten.Text = selectedCustomer.ho_ten;
+                txtDiaChi.Text = selectedCustomer.dia_chi;
+                txtSoDienThoai.Text = selectedCustomer.so_dien_thoai;
+                dtNgaySinh.Value = selectedCustomer.ngay_sinh;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (txtHoten.Text.Length <= 0 || txtDiaChi.Text.Length <= 0)
+            {
+                MessageBox.Show("Yêu cầu nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK);
+                return;
+            }
+            try
+            {
+                double phone_number = double.Parse(txtSoDienThoai.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Số điện thoại phải là số!", "Thông báo", MessageBoxButtons.OK);
+                return;
+            }
+            if (selectedCustomer == null)
+            {
+                khach_hang entity = new khach_hang();
+                entity.ho_ten = txtHoten.Text;
+                entity.ngay_sinh = dtNgaySinh.Value;
+                entity.dia_chi = txtDiaChi.Text;
+                entity.so_dien_thoai = txtSoDienThoai.Text;
+                db.khach_hang.Add(entity);
+                db.SaveChanges();
+                MessageBox.Show("Thêm dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
+            }
+            else
+            {
+                khach_hang entity = db.khach_hang.Find(selectedCustomer.ma_khach_hang);
+                entity.ho_ten = txtHoten.Text;
+                entity.ngay_sinh = dtNgaySinh.Value;
+                entity.dia_chi = txtDiaChi.Text;
+                entity.so_dien_thoai = txtSoDienThoai.Text;
+                db.SaveChanges();
+                MessageBox.Show("Chỉnh sửa dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
+            }
+            this.Hide();
+        }
+    }
+}
