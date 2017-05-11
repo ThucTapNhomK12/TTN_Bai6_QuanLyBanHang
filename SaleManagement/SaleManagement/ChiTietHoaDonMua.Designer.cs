0f3c7552e088f34692180bbeec42f5d86fdfd6c3
naq259 <naq259@gmail.com>
2017-05-11 23:07:22 +0800

    ChiTietHoaDonMua

---
+++ SaleManagement/SaleManagement/ChiTietHoaDonMua.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace SaleManagement
+{
+    partial class ChiTietHoaDonMua
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblTitle = new DevComponents.DotNetBar.LabelX();
+            this.dgvDetail = new DevComponents.DotNetBar.Controls.DataGridViewX();
+            this.ma_san_pham = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.so_luong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.don_gia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.thanh_tien = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ma_hoa_don = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hoa_don = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.san_pham_column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.txtQuantity = new DevComponents.Editors.IntegerInput();
+            this.cbProduct = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.btnAdd = new DevComponents.DotNetBar.ButtonX();
+            this.btnDelete = new DevComponents.DotNetBar.ButtonX();
+            this.btnSave = new DevComponents.DotNetBar.ButtonX();
+            this.btnExit = new DevComponents.DotNetBar.ButtonX();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQuantity)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            // 
+            // 
+            // 
+            this.lblTitle.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(16, 15);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(444, 28);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Mã hóa đơn";
+            // 
+            // dgvDetail
+            // 
+            this.dgvDetail.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetail.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ma_san_pham,
+            this.so_luong,
+            this.don_gia,
+            this.thanh_tien,
+            this.ma_hoa_don,
+            this.hoa_don,
+            this.san_pham_column});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvDetail.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvDetail.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(215)))), ((int)(((byte)(229)))));
+            this.dgvDetail.Location = new System.Drawing.Point(16, 50);
+            this.dgvDetail.Name = "dgvDetail";
+            this.dgvDetail.ReadOnly = true;
+            this.dgvDetail.Size = new System.Drawing.Size(453, 167);
+            this.dgvDetail.TabIndex = 1;
+            this.dgvDetail.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgvDetail_CellFormatting);
+            this.dgvDetail.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvDetail_CellMouseClick);
+            // 
+            // ma_san_pham
+            // 
+            this.ma_san_pham.DataPropertyName = "ma_san_pham";
+            this.ma_san_pham.HeaderText = "Sản phẩm";
+            this.ma_san_pham.Name = "ma_san_pham";
+            this.ma_san_pham.ReadOnly = true;
+            // 
+            // so_luong
+            // 
+            this.so_luong.DataPropertyName = "so_luong";
+            this.so_luong.HeaderText = "Số lượng";
+            this.so_luong.Name = "so_luong";
+            this.so_luong.ReadOnly = true;
+            // 
+            // don_gia
+            // 
+            this.don_gia.DataPropertyName = "don_gia";
+            this.don_gia.HeaderText = "Đơn giá";
+            this.don_gia.Name = "don_gia";
+            this.don_gia.ReadOnly = true;
+            // 
+            // thanh_tien
+            // 
+            this.thanh_tien.DataPropertyName = "thanh_tien";
+            this.thanh_tien.HeaderText = "Thành tiền";
+            this.thanh_tien.Name = "thanh_tien";
+            this.thanh_tien.ReadOnly = true;
+            // 
+            // ma_hoa_don
+            // 
+            this.ma_hoa_don.DataPropertyName = "ma_hoa_don";
+            this.ma_hoa_don.HeaderText = "Mã hóa đơn";
+            this.ma_hoa_don.Name = "ma_hoa_don";
+            this.ma_hoa_don.ReadOnly = true;
+            this.ma_hoa_don.Visible = false;
+            // 
+            // hoa_don
+            // 
+            this.hoa_don.DataPropertyName = "hoa_don_mua";
+            this.hoa_don.HeaderText = "Hóa đơn";
+            this.hoa_don.Name = "hoa_don";
+            this.hoa_don.ReadOnly = true;
+            this.hoa_don.Visible = false;
+            // 
+            // san_pham_column
+            // 
+            this.san_pham_column.DataPropertyName = "san_pham";
+            this.san_pham_column.HeaderText = "Sản phẩm";
+            this.san_pham_column.Name = "san_pham_column";
+            this.san_pham_column.ReadOnly = true;
+            this.san_pham_column.Visible = false;
+            // 
+            // labelX2
+            // 
+            // 
+            // 
+            // 
+            this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX2.Location = new System.Drawing.Point(78, 240);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(75, 22);
+            this.labelX2.TabIndex = 2;
+            this.labelX2.Text = "Sản phẩm";
+            // 
+            // labelX3
+            // 
+            // 
+            // 
+            // 
+            this.labelX3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX3.Location = new System.Drawing.Point(78, 277);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(75, 22);
+            this.labelX3.TabIndex = 4;
+            this.labelX3.Text = "Số lượng";
+            // 
+            // txtQuantity
+            // 
+            // 
+            // 
+            // 
+            this.txtQuantity.BackgroundStyle.Class = "DateTimeInputBackground";
+            this.txtQuantity.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtQuantity.ButtonFreeText.Shortcut = DevComponents.DotNetBar.eShortcut.F2;
+            this.txtQuantity.InputHorizontalAlignment = DevComponents.Editors.eHorizontalAlignment.Center;
+            this.txtQuantity.Location = new System.Drawing.Point(168, 277);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.ShowUpDown = true;
+            this.txtQuantity.Size = new System.Drawing.Size(204, 22);
+            this.txtQuantity.TabIndex = 5;
+            // 
+            // cbProduct
+            // 
+            this.cbProduct.DisplayMember = "Text";
+            this.cbProduct.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbProduct.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbProduct.FormattingEnabled = true;
+            this.cbProduct.ItemHeight = 16;
+            this.cbProduct.Location = new System.Drawing.Point(168, 240);
+            this.cbProduct.Name = "cbProduct";
+            this.cbProduct.Size = new System.Drawing.Size(204, 22);
+            this.cbProduct.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.cbProduct.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnAdd.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnAdd.Location = new System.Drawing.Point(27, 320);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 31);
+            this.btnAdd.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnDelete.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnDelete.Location = new System.Drawing.Point(146, 320);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 31);
+            this.btnDelete.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnSave.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnSave.Location = new System.Drawing.Point(260, 320);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 31);
+            this.btnSave.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnExit.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnExit.Location = new System.Drawing.Point(376, 320);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 31);
+            this.btnExit.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // ChiTietHoaDonMua
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 387);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbProduct);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.dgvDetail);
+            this.Controls.Add(this.lblTitle);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ChiTietHoaDonMua";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chi tiết hóa đơn mua";
+            this.Load += new System.EventHandler(this.ChiTietHoaDonMua_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtQuantity)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.LabelX lblTitle;
+        private DevComponents.DotNetBar.Controls.DataGridViewX dgvDetail;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private DevComponents.DotNetBar.LabelX labelX3;
+        private DevComponents.Editors.IntegerInput txtQuantity;
+        private DevComponents.DotNetBar.Controls.ComboBoxEx cbProduct;
+        private DevComponents.DotNetBar.ButtonX btnAdd;
+        private DevComponents.DotNetBar.ButtonX btnDelete;
+        private DevComponents.DotNetBar.ButtonX btnSave;
+        private DevComponents.DotNetBar.ButtonX btnExit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ma_san_pham;
+        private System.Windows.Forms.DataGridViewTextBoxColumn so_luong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn don_gia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn thanh_tien;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ma_hoa_don;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hoa_don;
+        private System.Windows.Forms.DataGridViewTextBoxColumn san_pham_column;
+    }
+}
---
+++ SaleManagement/SaleManagement/ChiTietHoaDonMua.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaleManagement
+{
+    public partial class ChiTietHoaDonMua : Form
+    {
+        db_sale_managementEntities db = new db_sale_managementEntities();
+
+        public static hoa_don_mua selectedBuyInvoice = null;
+
+        public ChiTietHoaDonMua()
+        {
+            InitializeComponent();
+        }
+
+        private void ChiTietHoaDonMua_Load(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        public void load()
+        {
+            selectedBuyInvoice = QuanLy.selectedBuyInvoice;
+            dgvDetail.DataSource = db.chi_tiet_hoa_don_mua.Where(x => x.ma_hoa_don == selectedBuyInvoice.ma_hoa_don).ToList();
+            lblTitle.Text = "Mã hóa đơn: " + selectedBuyInvoice.ma_hoa_don;
+
+            cbProduct.DataSource = db.san_pham.ToList();
+            cbProduct.DisplayMember = "ten_san_pham";
+            cbProduct.ValueMember = "ma_san_pham";
+
+            cbProduct.Enabled = false;
+            txtQuantity.Enabled = false;
+            btnDelete.Enabled = false;
+            btnSave.Enabled = false;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            List<chi_tiet_hoa_don_mua> listDetail = db.chi_tiet_hoa_don_mua.Where(x => x.ma_hoa_don == selectedBuyInvoice.ma_hoa_don).ToList();
+            List<int> listProductId = new List<int>();
+            foreach (chi_tiet_hoa_don_mua item in listDetail)
+            {
+                listProductId.Add(item.ma_san_pham);
+            }
+            List<san_pham> list = db.san_pham.Where(x => !listProductId.Contains(x.ma_san_pham)).ToList();
+            if (list.Count > 0)
+            {
+                cbProduct.DataSource = list;
+                cbProduct.DisplayMember = "ten_san_pham";
+                cbProduct.ValueMember = "ma_san_pham";
+            }
+            txtQuantity.Text = null;
+            cbProduct.Enabled = true;
+            txtQuantity.Enabled = true;
+            btnDelete.Enabled = false;
+            btnSave.Enabled = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int quantity = txtQuantity.Value;
+                if (quantity <= 0)
+                {
+                    MessageBox.Show("Số lượng phải là số nguyên dương!", "Thông báo", MessageBoxButtons.OK);
+                    return;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Yêu cầu nhập đúng định dạng của thông tin!", "Thông báo", MessageBoxButtons.OK);
+            }
+            try
+            {
+                san_pham product = db.san_pham.Find(int.Parse(cbProduct.SelectedValue.ToString()));
+                //Thêm chi tiết hóa đơn
+                chi_tiet_hoa_don_mua entity = new chi_tiet_hoa_don_mua();
+                entity.ma_hoa_don = selectedBuyInvoice.ma_hoa_don;
+                entity.ma_san_pham = product.ma_san_pham;
+                entity.so_luong = txtQuantity.Value;
+                entity.don_gia = product.gia_san_pham;
+                entity.thanh_tien = entity.so_luong * entity.don_gia;
+                db.chi_tiet_hoa_don_mua.Add(entity);
+                db.SaveChanges();
+                //Cập nhật số lượng sản phẩm
+                product.so_luong += entity.so_luong;
+                product.tinh_trang = (product.so_luong > 0) ? true : false;
+                db.SaveChanges();
+                //Cập nhật tổng tiền của hóa đơn
+                hoa_don_mua buyInvoice = db.hoa_don_mua.Find(selectedBuyInvoice.ma_hoa_don);
+                List<chi_tiet_hoa_don_mua> listDetail = db.chi_tiet_hoa_don_mua.Where(x => x.ma_hoa_don == selectedBuyInvoice.ma_hoa_don).ToList();
+                double sum = 0;
+                foreach (chi_tiet_hoa_don_mua item in listDetail)
+                {
+                    sum += item.thanh_tien;
+                }
+                buyInvoice.tong_tien = sum;
+                db.SaveChanges();
+                load();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Sản phẩm không tồn tại hoặc đã có trong hóa đơn!", "Thông báo", MessageBoxButtons.OK);
+            }
+            
+        }
+
+        private void dgvDetail_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            try
+            {
+                load();
+                cbProduct.SelectedValue = dgvDetail.Rows[e.RowIndex].Cells[1].Value;
+                txtQuantity.Text = dgvDetail.Rows[e.RowIndex].Cells[2].Value.ToString();
+                btnDelete.Enabled = true;
+                btnSave.Enabled = false;
+                cbProduct.Enabled = false;
+                txtQuantity.Enabled = false;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Click chuột sai vị trí", "Lỗi", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            san_pham product = db.san_pham.Find(int.Parse(cbProduct.SelectedValue.ToString()));
+            chi_tiet_hoa_don_mua entity = db.chi_tiet_hoa_don_mua.SingleOrDefault(x => x.ma_hoa_don == selectedBuyInvoice.ma_hoa_don && x.ma_san_pham == product.ma_san_pham);
+            db.chi_tiet_hoa_don_mua.Remove(entity);
+            db.SaveChanges();
+            //Cập nhật tổng tiền của hóa đơn
+            hoa_don_mua buyInvoice = db.hoa_don_mua.Find(selectedBuyInvoice.ma_hoa_don);
+            List<chi_tiet_hoa_don_mua> listDetail = db.chi_tiet_hoa_don_mua.Where(x => x.ma_hoa_don == selectedBuyInvoice.ma_hoa_don).ToList();
+            double sum = 0;
+            foreach (chi_tiet_hoa_don_mua item in listDetail)
+            {
+                sum += item.thanh_tien;
+            }
+            buyInvoice.tong_tien = sum;
+            db.SaveChanges();
+            MessageBox.Show("Xóa dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK);
+            load();
+        }
+
+        private void dgvDetail_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (this.dgvDetail.Columns[e.ColumnIndex].Name == "ma_san_pham")
+            {
+                if (e.Value != null)
+                {
+                    san_pham product = db.san_pham.Find(e.Value);
+                    e.Value = product.ten_san_pham;
+                }
+            }
+        }
+    }
+}
