03b01720e95206337057f99479c184a2c001fe8e
BuiMaiHuong <huongbt811@gmail.com>
2017-05-18 14:46:48 +0700

    form nhan vien

---
+++ SaleManagement/SaleManagement/NhanVienForm.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace SaleManagement
+{
+    partial class NhanVienForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            this.labelX2 = new DevComponents.DotNetBar.LabelX();
+            this.txtHoten = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.txtDiaChi = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX3 = new DevComponents.DotNetBar.LabelX();
+            this.txtSoDienThoai = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX4 = new DevComponents.DotNetBar.LabelX();
+            this.txtTaiKhoan = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX5 = new DevComponents.DotNetBar.LabelX();
+            this.labelX7 = new DevComponents.DotNetBar.LabelX();
+            this.cbTrangThai = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.cbPhanQuyen = new DevComponents.DotNetBar.Controls.ComboBoxEx();
+            this.labelX8 = new DevComponents.DotNetBar.LabelX();
+            this.btnSubmit = new DevComponents.DotNetBar.ButtonX();
+            this.btnExit = new DevComponents.DotNetBar.ButtonX();
+            this.txtMatKhau = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX6 = new DevComponents.DotNetBar.LabelX();
+            this.SuspendLayout();
+            // 
+            // labelX1
+            // 
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX1.Location = new System.Drawing.Point(118, 13);
+            this.labelX1.Margin = new System.Windows.Forms.Padding(4);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(198, 28);
+            this.labelX1.TabIndex = 0;
+            this.labelX1.Text = "Thông tin nhân viên";
+            // 
+            // labelX2
+            // 
+            // 
+            // 
+            // 
+            this.labelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX2.Location = new System.Drawing.Point(56, 63);
+            this.labelX2.Name = "labelX2";
+            this.labelX2.Size = new System.Drawing.Size(87, 22);
+            this.labelX2.TabIndex = 1;
+            this.labelX2.Text = "Họ tên";
+            // 
+            // txtHoten
+            // 
+            // 
+            // 
+            // 
+            this.txtHoten.Border.Class = "TextBoxBorder";
+            this.txtHoten.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtHoten.Location = new System.Drawing.Point(158, 63);
+            this.txtHoten.Name = "txtHoten";
+            this.txtHoten.PreventEnterBeep = true;
+            this.txtHoten.Size = new System.Drawing.Size(203, 22);
+            this.txtHoten.TabIndex = 0;
+            // 
+            // txtDiaChi
+            // 
+            // 
+            // 
+            // 
+            this.txtDiaChi.Border.Class = "TextBoxBorder";
+            this.txtDiaChi.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtDiaChi.Location = new System.Drawing.Point(158, 104);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.PreventEnterBeep = true;
+            this.txtDiaChi.Size = new System.Drawing.Size(203, 22);
+            this.txtDiaChi.TabIndex = 1;
+            // 
+            // labelX3
+            // 
+            // 
+            // 
+            // 
+            this.labelX3.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX3.Location = new System.Drawing.Point(56, 104);
+            this.labelX3.Name = "labelX3";
+            this.labelX3.Size = new System.Drawing.Size(87, 22);
+            this.labelX3.TabIndex = 3;
+            this.labelX3.Text = "Địa chỉ";
+            // 
+            // txtSoDienThoai
+            // 
+            // 
+            // 
+            // 
+            this.txtSoDienThoai.Border.Class = "TextBoxBorder";
+            this.txtSoDienThoai.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtSoDienThoai.Location = new System.Drawing.Point(158, 147);
+            this.txtSoDienThoai.Name = "txtSoDienThoai";
+            this.txtSoDienThoai.PreventEnterBeep = true;
+            this.txtSoDienThoai.Size = new System.Drawing.Size(203, 22);
+            this.txtSoDienThoai.TabIndex = 2;
+            // 
+            // labelX4
+            // 
+            // 
+            // 
+            // 
+            this.labelX4.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX4.Location = new System.Drawing.Point(56, 147);
+            this.labelX4.Name = "labelX4";
+            this.labelX4.Size = new System.Drawing.Size(87, 22);
+            this.labelX4.TabIndex = 5;
+            this.labelX4.Text = "Số điện thoại";
+            // 
+            // txtTaiKhoan
+            // 
+            // 
+            // 
+            // 
+            this.txtTaiKhoan.Border.Class = "TextBoxBorder";
+            this.txtTaiKhoan.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtTaiKhoan.Location = new System.Drawing.Point(158, 189);
+            this.txtTaiKhoan.Name = "txtTaiKhoan";
+            this.txtTaiKhoan.PreventEnterBeep = true;
+            this.txtTaiKhoan.Size = new System.Drawing.Size(203, 22);
+            this.txtTaiKhoan.TabIndex = 3;
+            // 
+            // labelX5
+            // 
+            // 
+            // 
+            // 
+            this.labelX5.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX5.Location = new System.Drawing.Point(56, 189);
+            this.labelX5.Name = "labelX5";
+            this.labelX5.Size = new System.Drawing.Size(87, 22);
+            this.labelX5.TabIndex = 7;
+            this.labelX5.Text = "Tài khoản";
+            // 
+            // labelX7
+            // 
+            // 
+            // 
+            // 
+            this.labelX7.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX7.Location = new System.Drawing.Point(56, 279);
+            this.labelX7.Name = "labelX7";
+            this.labelX7.Size = new System.Drawing.Size(87, 20);
+            this.labelX7.TabIndex = 11;
+            this.labelX7.Text = "Trạng thái";
+            // 
+            // cbTrangThai
+            // 
+            this.cbTrangThai.DisplayMember = "Text";
+            this.cbTrangThai.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbTrangThai.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbTrangThai.FormattingEnabled = true;
+            this.cbTrangThai.ItemHeight = 16;
+            this.cbTrangThai.Location = new System.Drawing.Point(158, 277);
+            this.cbTrangThai.Name = "cbTrangThai";
+            this.cbTrangThai.Size = new System.Drawing.Size(203, 22);
+            this.cbTrangThai.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.cbTrangThai.TabIndex = 5;
+            // 
+            // cbPhanQuyen
+            // 
+            this.cbPhanQuyen.DisplayMember = "Text";
+            this.cbPhanQuyen.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cbPhanQuyen.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPhanQuyen.FormattingEnabled = true;
+            this.cbPhanQuyen.ItemHeight = 16;
+            this.cbPhanQuyen.Location = new System.Drawing.Point(158, 323);
+            this.cbPhanQuyen.Name = "cbPhanQuyen";
+            this.cbPhanQuyen.Size = new System.Drawing.Size(203, 22);
+            this.cbPhanQuyen.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.cbPhanQuyen.TabIndex = 6;
+            // 
+            // labelX8
+            // 
+            // 
+            // 
+            // 
+            this.labelX8.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX8.Location = new System.Drawing.Point(56, 325);
+            this.labelX8.Name = "labelX8";
+            this.labelX8.Size = new System.Drawing.Size(87, 20);
+            this.labelX8.TabIndex = 13;
+            this.labelX8.Text = "Phân quyền";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnSubmit.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnSubmit.Location = new System.Drawing.Point(158, 372);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(87, 31);
+            this.btnSubmit.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "Xác nhận";
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnExit.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnExit.Location = new System.Drawing.Point(273, 372);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(87, 31);
+            this.btnExit.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtMatKhau
+            // 
+            // 
+            // 
+            // 
+            this.txtMatKhau.Border.Class = "TextBoxBorder";
+            this.txtMatKhau.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.txtMatKhau.Location = new System.Drawing.Point(158, 232);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.PasswordChar = '*';
+            this.txtMatKhau.PreventEnterBeep = true;
+            this.txtMatKhau.Size = new System.Drawing.Size(203, 22);
+            this.txtMatKhau.TabIndex = 4;
+            // 
+            // labelX6
+            // 
+            // 
+            // 
+            // 
+            this.labelX6.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX6.Location = new System.Drawing.Point(56, 232);
+            this.labelX6.Name = "labelX6";
+            this.labelX6.Size = new System.Drawing.Size(87, 22);
+            this.labelX6.TabIndex = 19;
+            this.labelX6.Text = "Mật khẩu";
+            // 
+            // NhanVienForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 429);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.labelX6);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.cbPhanQuyen);
+            this.Controls.Add(this.labelX8);
+            this.Controls.Add(this.cbTrangThai);
+            this.Controls.Add(this.labelX7);
+            this.Controls.Add(this.txtTaiKhoan);
+            this.Controls.Add(this.labelX5);
+            this.Controls.Add(this.txtSoDienThoai);
+            this.Controls.Add(this.labelX4);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.labelX3);
+            this.Controls.Add(this.txtHoten);
+            this.Controls.Add(this.labelX2);
+            this.Controls.Add(this.labelX1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "NhanVienForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nhân viên";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.LabelX labelX1;
+        private DevComponents.DotNetBar.LabelX labelX2;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtHoten;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtDiaChi;
+        private DevComponents.DotNetBar.LabelX labelX3;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtSoDienThoai;
+        private DevComponents.DotNetBar.LabelX labelX4;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtTaiKhoan;
+        private DevComponents.DotNetBar.LabelX labelX5;
+        private DevComponents.DotNetBar.LabelX labelX7;
+        private DevComponents.DotNetBar.Controls.ComboBoxEx cbTrangThai;
+        private DevComponents.DotNetBar.Controls.ComboBoxEx cbPhanQuyen;
+        private DevComponents.DotNetBar.LabelX labelX8;
+        private DevComponents.DotNetBar.ButtonX btnSubmit;
+        private DevComponents.DotNetBar.ButtonX btnExit;
+        private DevComponents.DotNetBar.Controls.TextBoxX txtMatKhau;
+        private DevComponents.DotNetBar.LabelX labelX6;
+    }
+}
---
+++ SaleManagement/SaleManagement/NhanVienForm.cs
@@ -0,0 +1,35 @@
+﻿using SaleManagement.Common;
+using SaleManagement.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaleManagement
+{
+    public partial class NhanVienForm : Form
+    {
+        
+        }
+
+        public void loadForm()
+        {
+           }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
