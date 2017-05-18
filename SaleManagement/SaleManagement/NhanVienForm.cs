88867f1f3496c6877a98259484935f04c963f144
BuiMaiHuong <huongbt811@gmail.com>
2017-05-18 14:48:29 +0700

    update nhan vien form

--- SaleManagement/SaleManagement/NhanVienForm.cs
+++ SaleManagement/SaleManagement/NhanVienForm.cs
@@ -14,7 +14,24 @@ namespace SaleManagement
 {
     public partial class NhanVienForm : Form
     {
-        
+        db_sale_managementEntities db = new db_sale_managementEntities();
+
+        public nhan_vien selectedStaff = QuanLy.selectedStaff;
+        public NhanVienForm()
+        {
+            InitializeComponent();
+            loadForm();
+            if (selectedStaff != null)
+            {
+                txtHoten.Text = selectedStaff.ho_ten;
+                txtDiaChi.Text = selectedStaff.dia_chi;
+                txtSoDienThoai.Text = selectedStaff.so_dien_thoai;
+                txtTaiKhoan.Text = selectedStaff.tai_khoan;
+                txtMatKhau.Text = selectedStaff.mat_khau;
+                cbTrangThai.SelectedValue = (selectedStaff.trang_thai == true) ? 1 : 0;
+                cbPhanQuyen.SelectedValue = selectedStaff.phan_quyen;
+                txtTaiKhoan.Enabled = false;
+            }
         }
 
         public void loadForm()
